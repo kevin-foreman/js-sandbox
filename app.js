3f32737f0e06a2d6fbee3dfdfc54a6cae3020a15
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-01-03 08:43:53 -0600

    Repeat string method

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-// function assertArraysEqual(actual, expected, testName) {
-    // your code here
+function repeatString(string, num) {
+  // your code here
+return string.repeat(num);
+};
 
-// };
-
-// var expected = ['b', 'r', 'o', 'k', 'e', 'n'];
-// var actual = 'broken'.split('');
-// assertArraysEqual(actual, expected, 'splits string into array of characters');
-// console output:
-// passed
-
-// var expected = [1, 1, 2, 3, 5, 8, 13];
-// var actual = generateFirstNFibonaccis(7);
-// assertArraysEqual(actual, expected, 'generates first 7 Fibonacci numbers');
-// console output:
-// FAILED [generates first 7 Fibonacci numbers] Expected "1, 1, 2, 3, 5, 8, 13", but got "2, 3, 5, 8, 13, 21, 34"
-
-var scalar1 = 'equal';
-var scalar2 = 'equal';
-console.log("two scalars can be compared with each other using triple equals:,");
-console.log(scalar1 === scalar2);
-
-var array1 = ['a', 'b', 'c'];
-var array2 = ['a', 'b', 'c'];
-console.log("two arrays can be compared with each other using triple equals");
-console.log(array1 === array2);
+var output = repeatString('code', 3);
+console.log(output); // --> 'codecodecode'
