a093566d559f7441dc9da5c48c646e827ead5f7a
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-01-05 10:40:42 -0600

    Assert Arrays Equal with sample function and pass/fail tests successful

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 function assertArraysEqual(actual, expected, testName) {
-    // your code here
+
+    var lengthsEqual = (actual.length === expected.length)
+    var contentsEqual = true
+    for (var i = 0; i < actual.length; i++) {
+
+        if (actual[i] !== expected[i]) {
+            
+            contentsEqual = false
+        
+        };
+
+    }
+
+    if (contentsEqual && lengthsEqual) {
+
+        console.log("passed")
+
+    } else {
+        console.log('FAILED [' + testName + '] Expected "' + expected + '", but got "' + actual + '"');
+    };
 };
 
 // Success case
@@ -9,9 +28,16 @@ assertArraysEqual(actual, expected, 'splits string into array of characters');
 // console output:
 // passed
 
-// Failure case
+function generateFirstNFibonaccis(n) {
+    var arr = [0, 1];
+    for (var i = 2; i < n; i++) {
+        arr.push(arr[i - 2] + arr[i - 1]);
+    }
+    return arr;
+};
+
 var expected = [1, 1, 2, 3, 5, 8, 13];
 var actual = generateFirstNFibonaccis(7);
 assertArraysEqual(actual, expected, 'generates first 7 Fibonacci numbers');
 // console output:
-// FAILED [generates first 7 Fibonacci numbers] Expected "1, 1, 2, 3, 5, 8, 13", but got "2, 3, 5, 8, 13, 21, 34"
+// FAILED [generates first 7 Fibonacci numbers] Expected "1, 1, 2, 3, 5, 8, 13", but got "0, 1, 1 ,2 ,3 ,5 ,8"
